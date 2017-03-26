61a763bcde032a0c15d95ee89d78be02b446c97a
ebpproject <ebpproject1975@gmail.com>
2017-03-26 19:58:09 +0200

    creacion de reset.scc

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ebp-grid
+
+## reset.css
+
+
+With the reset.css style sheet we can reset our html.
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,9 @@
 'use strict';
-const  gulp = require('gulp'),
+const gulp = require('gulp'),
     pug = require('gulp-pug'),
     sass = require('gulp-sass'),
     babel = require('gulp-babel'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    svgmin = require('gulp-svgmin'),
-    webp = require('gulp-webp'),
-    useref = require('gulp-useref'),
-    concat = require('gulp-concat'),
-    uncss = require('gulp-uncss'),
-    autoprefixer = require('gulp-autoprefixer'),
-    cleanCSS = require('gulp-clean-css'),
-    uglify = require('gulp-uglify'),
-    htmlmin = require('gulp-htmlmin'),
+
     /** Paths of application directories */
     dir = {
         src: 'src',
@@ -24,54 +14,55 @@ const  gulp = require('gulp'),
      *
      * @files
      */
-    files ={
+    files = {
         /**
          * Go all css files
          */
-        CSS:[
+        CSS: [
+            `${dir.public}/css/reset.css`,
             `${dir.public}/css/styles.css`
         ],
         /**
          * Go all js files
          */
-        JS:[
-           `${dir.public}/js/app.js`
+        JS: [
+            `${dir.public}/js/app.js`
         ]
     },
-    opts ={
-        pug:{
-            pretty : true,
-            locals : {
+    opts = {
+        pug: {
+            pretty: true,
+            locals: {
                 title: 'Ebp-grid',
-                files : files
+                files: files
             }
         },
-        sass:{ outputStyle: 'compressed'},
-        es6: {presets: ['es2015']}
+        sass: { outputStyle: 'compressed' },
+        es6: { presets: ['es2015'] }
     };
 
-    /**
-    *Develop gulp to build pug to html
-    */
+/**
+ *Develop gulp to build pug to html
+ */
 
-    gulp.task('pug', ()=> {
-        gulp
-            .src(`${dir.src}/pug/*.pug`)
-            .pipe(pug(opts.pug))
-            .pipe(gulp.dest(dir.public));
-    });
-    /**
-    *Develop gulp to build sass to css
-    */
+gulp.task('pug', () => {
+    gulp
+        .src(`${dir.src}/pug/*.pug`)
+        .pipe(pug(opts.pug))
+        .pipe(gulp.dest(dir.public));
+});
+/**
+ *Develop gulp to build sass to css
+ */
 
-    gulp.task('sass', ()=> {
-        gulp
-            .src(`${dir.src}/scss/*.scss`)
-            .pipe(sass(opts.sas))
-            .pipe(gulp.dest(`${dir.public}/css`));
-    });
+gulp.task('sass', () => {
+    gulp
+        .src(`${dir.src}/scss/*.scss`)
+        .pipe(sass(opts.sas))
+        .pipe(gulp.dest(`${dir.public}/css`));
+});
 
-gulp.task('es6', ()=> {
+gulp.task('es6', () => {
     gulp
         .src(`${dir.src}/es6/*.js`)
         .pipe(babel(opts.es6))
